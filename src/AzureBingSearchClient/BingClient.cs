b700ce3d8ce76034ed9277cde68bf03cea8a8452
hiromasa masuda <hiromasa.masuda@live.jp>
2024-02-25 22:28:14 +0900

    Separated the model classes into separate files.

--- src/AzureBingSearchClient/BingClient.cs
+++ src/AzureBingSearchClient/BingClient.cs
@@ -1,57 +1,11 @@
-﻿using System.Text.Json;
+﻿using AzureBingSearchClient.Models;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace AzureBingSearchClient;
 
 public class BingClient
 {
-    public class SerachResult
-    {
-        public JsonNode? JsonNode;
-    }
-
-    // Web ページ検索結果
-    public class WebPage : SerachResult
-    {
-        public string? Name;
-        public string? Url;
-        public string? Snippet;
-        public DateTimeOffset? DateLastCrawled;
-    }
-
-    // 画像検索結果
-    public class WebImage : SerachResult
-    {
-        public string? Name;
-        public string? ThumbnailUrl;
-        public string? ContentUrl;
-        public DateTimeOffset? DatePublished;
-    }
-
-    // 動画検索結果
-    public class WebVideo : SerachResult
-    {
-        public string? Name;
-        public string? ThumbnailUrl;
-        public string? ContentUrl;
-        public string? Description;
-        public string? Publisher;
-        public string? Creator;
-        public long? ViewCount;
-        public DateTimeOffset? DatePublished;
-    }
-
-    // News 検索結果
-    public class WebNews : SerachResult
-    {
-        public string? Name;
-        public string? Url;
-        public string? Description;
-        public string? ThumbnailUrl;
-        public string? Category;
-        public DateTimeOffset? DatePublished;
-    }
-
     private static readonly HttpClient _httpClient = new HttpClient();
 
     private readonly string _endpoint = "https://api.bing.microsoft.com/v7.0";
---
+++ src/AzureBingSearchClient/Models/SerachResult.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Nodes;
+
+namespace AzureBingSearchClient.Models;
+
+public class SerachResult
+{
+    public JsonNode? JsonNode;
+}
+
+// Web ページ検索結果
+public class WebPage : SerachResult
+{
+    public string? Name;
+    public string? Url;
+    public string? Snippet;
+    public DateTimeOffset? DateLastCrawled;
+}
+
+// 画像検索結果
+public class WebImage : SerachResult
+{
+    public string? Name;
+    public string? ThumbnailUrl;
+    public string? ContentUrl;
+    public DateTimeOffset? DatePublished;
+}
+
+// 動画検索結果
+public class WebVideo : SerachResult
+{
+    public string? Name;
+    public string? ThumbnailUrl;
+    public string? ContentUrl;
+    public string? Description;
+    public string? Publisher;
+    public string? Creator;
+    public long? ViewCount;
+    public DateTimeOffset? DatePublished;
+}
+
+// News 検索結果
+public class WebNews : SerachResult
+{
+    public string? Name;
+    public string? Url;
+    public string? Description;
+    public string? ThumbnailUrl;
+    public string? Category;
+    public DateTimeOffset? DatePublished;
+}
